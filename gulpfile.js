e9449062fbf470f407ded3cc37ff917c9ac4b512
Dmitry Novoselov <dmitry.a.novoselov@yandex.ru>
2016-06-05 08:10:44 +0300

    include templates into page

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
+var run = require('run-sequence');
 
 var replace = require('gulp-replace');
 var rename = require("gulp-rename");
 var watch = require('gulp-watch');
+var wrap = require('gulp-wrap');
 var concat = require('gulp-concat');
+var include = require('gulp-include');
 
 var uglify = require('gulp-uglify');
 var cleanCss = require('gulp-clean-css');
@@ -19,29 +22,39 @@ gulp.task("default", function() {
 
 var pathDest = "./ClientOnly.Build";
 var pathIndexCshtml = "./src/App/Views/App/MobileIndex.cshtml";
-var pathClientBase = "./src/App/Client/";
+var pathClientBase = "./src/App/Client";
 
 var pathBlocksHtml =  pathClientBase + "/blocks/**/*.html";
 var pathBlocksCss =  pathClientBase + "/blocks/**/*.css";
 
 var pathLibScripts = pathClientBase + "/scripts/lib";
 
-gulp.task("clientOnly", function() {
-    gulp.src(pathIndexCshtml)
-        .pipe(watch(pathIndexCshtml))
+gulp.task("clientOnly-concat-templates", function() {
+    return gulp.src(pathBlocksHtml)
+        .pipe(wrap({src: pathClientBase + "/blocks/template-wrap.txt"}))
+        .pipe(concat("templates.html"))
+        .pipe(gulp.dest(pathDest + "/client/blocks"));
+});
+
+gulp.task("clientOnly-build-index.html", ["clientOnly-concat-templates"], function() {
+    return gulp.src(pathIndexCshtml)
         .pipe(replace(/@using .*/g, ""))
         .pipe(replace(/@Styles.*/g, "<link rel='stylesheet' type='text/css' href='/css/app.css'>"))
         .pipe(replace(/@Scripts\.Render\("~\/js\/lib"\)/, "<script src='/js/lib.js'></script>"))
         .pipe(replace(/@Scripts\.Render\("~\/js\/app"\)/, "<script src='/js/app.js'></script>"))
+        .pipe(include({includePaths: pathDest}))
         .pipe(rename("index.html"))
         .pipe(gulp.dest(pathDest));
+});
 
-    gulp.src(pathBlocksHtml)
-        .pipe(watch(pathBlocksHtml))
-        .pipe(gulp.dest(pathDest + "/client/blocks"));
+gulp.task("clientOnly", ["clientOnly-build-index.html"], function() {
+    gulp.watch(pathIndexCshtml, ["clientOnly-build-index.html"]);
+    watch(pathBlocksHtml, function() {
+        return run("clientOnly-build-index.html");
+    });
 
     buildCss();
-    watch(pathClientBase + "/blocks/**/*.js", buildCss);
+    watch(pathClientBase + "/blocks/**/*.css", buildCss);
 
     gulp.src([
         pathLibScripts + "/jquery.min.js",
--- src/App/Client/scripts/program/init.js
+++ src/App/Client/scripts/program/init.js
@@ -1,13 +1,52 @@
 (function() {
 
-	modules.define("blocks", function(provide) {
-		provide({
-			names: [],
+	var blocks = {
+		names: [],
 
-			push: function(name) {
-				this.names.push(name);
-			}
-		});
+		push: function(name) {
+			this.names.push(name);
+		}
+	};
+
+	var templates = {};
+
+	blocks.renderBlock = function (blockName, model) {
+		var template = templates[blockName];
+
+		if (!template) {
+			var templateHtml = $("#" + blockName).html();
+			template = templates[blockName] = Handlebars.compile(templateHtml);
+		}
+
+		var html = template(model);
+
+		if (model && model.mix) {
+			var jNode = $(html);
+			jNode.addClass(model.mix);
+
+			html = jNode.get(0).outerHTML;
+		}
+
+		return html;
+	}
+
+	// custom Handlebars block
+	Handlebars.registerHelper("bem-block", function (blockName, options) {
+		return blocks.renderBlock(blockName, options.hash);
+	});
+
+	// jQuery plugin
+	$.fn.renderBlock = function (blockName, model) {
+		var html = blocks.renderBlock(blockName, model);
+		var jNode = $(html);
+
+		this.replaceWith(jNode);
+
+		return jNode.bem(blockName);
+	};
+
+	modules.define("blocks", function(provide) {
+		provide(blocks);
 	});
 
 })();
