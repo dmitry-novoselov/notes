18d06ac31b4df28e657b9f15723a0b0063fa61aa
Dmitry Novoselov <dmitry.a.novoselov@yandex.ru>
2016-07-24 15:20:56 +0300

    tried to improve authentication

--- src/App/Global.asax.cs
+++ src/App/Global.asax.cs
@@ -1,19 +1,40 @@
 ﻿using System;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace App
 {
 	public class Global : HttpApplication
 	{
-		void Application_Start(object sender, EventArgs e)
+		public Global()
 		{
-			// Code that runs on application startup
-			AreaRegistration.RegisterAllAreas();
-			GlobalConfiguration.Configure(WebApiConfig.Register);
+			AuthenticateRequest += OnAuthenticateRequest;
+			PostAuthenticateRequest += OnAuthenticateRequest;
+		}
+
+		private void Application_Start(object sender, EventArgs e)
+		{
+            GlobalConfiguration.Configure(WebApiConfig.Register);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		}
+
+		private void OnAuthenticateRequest(object sender, EventArgs eventArgs)
+		{
+			if (HttpContext.Current.Request.IsAuthenticated)
+				return;
+
+			var authenticationCoockie = Request.Cookies[FormsAuthentication.FormsCookieName];
+			if (authenticationCoockie == null)
+				return;
+
+			var ticket = FormsAuthentication.Decrypt(authenticationCoockie.Value);
+			if (ticket == null)
+				return;
+
+			HttpContext.Current.User = new GenericPrincipal(new FormsIdentity(ticket), new string[0]);
 		}
 	}
 }
