156e6d700fcb2ded9ef251b68140ee69cbd4ca2a
Dmitry Novoselov <dmitry.a.novoselov@yandex.ru>
2016-08-14 17:15:47 +0300

    switcher

--- src/App/Client/blocks/note-pane/note-pane.js
+++ src/App/Client/blocks/note-pane/note-pane.js
@@ -1,4 +1,4 @@
-modules.define("note-pane", ["i-bem__dom", "render"], function(provide, BEMDOM, render) {
+modules.define("note-pane", ["i-bem__dom", "render", "switcher"], function(provide, BEMDOM, render, Switcher) {
 
     /**
      * Events
@@ -6,21 +6,62 @@ modules.define("note-pane", ["i-bem__dom", "render"], function(provide, BEMDOM, 
      * - note-changed
      */
 
+	function textToHtml(text) {
+		return text
+			.replace(/(https?:\/\/[^\s]+)/g, "<a href='$1'>$1</a>")
+			.replace(/\n/g, "<br/>");
+	}
+
     provide(BEMDOM.decl(this.name,
         {
             onSetMod: {
                 "js": {
-                    "inited": function() {
-                    }
-                }
+                	"inited": function () {
+                		this.setMod("mode", "links");
+
+                		Switcher
+			                .on("switch-left", this._onSwitchedToLinks, this)
+			                .on("switch-right", this._onSwitchedToEditor, this);
+	                }
+                },
+
+				"mode": {
+					"links": function () {
+						this.findBlockInside(this.elem("switcher"), "switcher")
+							.setMod("position", "left");
+					},
+
+					"editor": function() {
+						this.findBlockInside(this.elem("switcher"), "switcher")
+							.setMod("position", "right");
+					}
+				}
             },
 
+        	_onSwitchedToLinks: function(e) {
+        		this.setMod("mode", "links");
+        	},
+
+        	_onSwitchedToEditor: function (e) {
+        		this.setMod("mode", "editor");
+        	},
+
+        	_updateLinksPane: function(text) {
+		        this.elem("links-area")
+			        .html(textToHtml(text));
+	        },
+
             // todo: unify block re-rendering
-            render: function(note) {
-                var html = render.blockContent("note-pane", {text: note.text});
+            render: function (note) {
+	            var noteText = note.text;
+	            var noteHtml = textToHtml(noteText);
+
+                var html = render.blockContent("note-pane", {text: noteText, html: noteHtml});
 
                 BEMDOM.update(this.domElem, html);
                 this.dropElemCache("text-area");
+
+	            this.setMod("mode", "links");
 
                 return this;
             }
@@ -36,8 +77,9 @@ modules.define("note-pane", ["i-bem__dom", "render"], function(provide, BEMDOM, 
                     clearTimeout(this._editTimeoutId);
 
                     var noteText = this.elem("text-area").val();
-                    this._editTimeoutId = setTimeout(function() {
-                        this.emit("note-changed", noteText);
+                    this._editTimeoutId = setTimeout(function () {
+                    	this._updateLinksPane(noteText);
+                    	this.emit("note-changed", noteText);
                     }.bind(this), 500); // todo: magic constant
                 });
             }
--- src/App/Client/blocks/switcher/switcher.js
+++ src/App/Client/blocks/switcher/switcher.js
@@ -8,12 +8,33 @@
 
     provide(BEMDOM.decl(this.name,
         {
-        	js: {
-				inited: function () {
-					this.on(this.elem("left"))
-				}
-			}
-        }
+        	_onSwitchedToLeftOption: function(e) {
+        		e.preventDefault();
+
+		        this.setMod("mode", "left");
+
+		        this.emit("switch-left");
+	        },
+
+        	_onSwitchedToRightOption: function (e) {
+        		e.preventDefault();
+
+        		this.setMod("mode", "right");
+
+		        this.emit("switch-right");
+	        }
+        },
+	    {
+	    	live: function () {
+		    	this.liveBindTo("left-option", "click touchstart", function(e) {
+				    this._onSwitchedToLeftOption(e);
+			    });
+
+		    	this.liveBindTo("right-option", "click touchstart", function(e) {
+				    this._onSwitchedToRightOption(e);
+			    });
+		    }
+	    }
     ));
 
 });
