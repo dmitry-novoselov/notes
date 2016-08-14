c1b53d32c181cfd3401f1f5c8b945ecf70350219
Dmitry Novoselov <dmitry.a.novoselov@yandex.ru>
2016-08-14 12:40:11 +0300

    switcher

---
+++ src/App/Client/blocks/switcher/switcher.js
@@ -0,0 +1,19 @@
+﻿modules.define("switcher", ["i-bem__dom"], function (provide, BEMDOM) {
+
+    /**
+     * Events
+     * - switch-left()
+     * - switch-right()
+     */
+
+    provide(BEMDOM.decl(this.name,
+        {
+        	js: {
+				inited: function () {
+					this.on(this.elem("left"))
+				}
+			}
+        }
+    ));
+
+});
--- src/App/Controllers/AppController.cs
+++ src/App/Controllers/AppController.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace App.Controllers
 {
@@ -13,24 +12,6 @@ namespace App.Controllers
 			}
 
 			return View("MobileIndex");
-		}
-
-		public ActionResult Coockies()
-		{
-			var sb = new StringBuilder();
-
-			foreach (var key in Request.Cookies.AllKeys)
-			{
-				var cookie = Request.Cookies[key];
-                sb.AppendFormat("{{'{0}' = '{1}'}} ", cookie.Name, cookie.Value);
-			}
-
-			return View(new CoockiesModel {Coockies = sb.ToString()});
-		}
-
-		public class CoockiesModel
-		{
-			public string Coockies;
 		}
 	}
 }
